1829a7f4b6c3982c09b7f86b56f6899056c5bbcb
lehuuvang1 <lehuuvang1@gmail.com>
2023-04-28 00:29:52 +0700

    commit project 28/04/2023

---
+++ VSLT_FastfoodTeam/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VSLT_FastfoodTeam.Models;
+
+namespace VSLT_FastfoodTeam.Controllers
+{
+    public class HomeController : Controller
+    {
+        MyDataDataContext data = new MyDataDataContext();
+        public ActionResult Index()
+        {
+            var all = (from s in data.SanPhams select s).OrderBy(m => m.MaSP);
+            return View(all);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
